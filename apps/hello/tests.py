d3a32c45ad37ed47c04d5ed68fc958b01927b982
jeyzth <jeyzth@gmail.com>
2016-03-01 23:04:59 +0200

    ticket:1 First test pass.    Fix PEP8 errors

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,27 +1,25 @@
 from django.test import TestCase
 from django.test.client import Client
 
-from hello.models import Applicant
-
 
 class SomeTests(TestCase):
     def test_math(self):
         "put docstrings in your tests"
         assert(2 + 2 == 4)
 
+
 class AppicantTest(TestCase):
     def test_get_mainpage(self):
         """  This test checks how view hard-coded data for the template
              on main page
         """
-        c=Client()
+        c = Client()
         responce = c.get('http://localhost:8080')
         s = responce.content
-        self.assertIn("42 Coffee Cups Test Assignment",s)
-        self.assertIn("Contacts",s)
-        self.assertIn("Email:",s)
-        self.assertIn("Jabber:",s)
-        self.assertIn("Skype:",s)
-        self.assertIn("Other contacts:",s)
-        self.assertIn("Bio:",s)
-    
+        self.assertIn("42 Coffee Cups Test Assignment", s)
+        self.assertIn("Contacts", s)
+        self.assertIn("Email:", s)
+        self.assertIn("Jabber:", s)
+        self.assertIn("Skype:", s)
+        self.assertIn("Other contacts:", s)
+        self.assertIn("Bio:", s)
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from hello.models import Applicant
 
 
 def index(request):
     try:
-        applicant = Applicant.objects.get(email = 'jeyzth@gmail.com')
+        applicant = Applicant.objects.get(email='jeyzth@gmail.com')
         context = {'applicant': applicant}
         return render(request, 'hello/index.html', context)
     except:
