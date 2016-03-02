f1a7934707e6778eae19c923567fc2b754d13508
jeyzth <jeyzth@gmail.com>
2016-03-02 22:19:57 +0200

    ticket:1 Done 4 Code the view, returning hard-coded data for the template and eploy

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 
-from hello.models import Applicant
-
 
 def index(request):
     try:
-        applicant = Applicant.objects.get(email='jeyzth@gmail.com')
-        context = {'applicant': applicant}
-        return render(request, 'hello/index.html', context)
+        return render(request, 'hello/index.html', None)
     except:
         return render(request, 'hello/emptytable.html', None)
