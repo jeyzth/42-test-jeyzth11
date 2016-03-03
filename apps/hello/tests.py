f3896049192f088410343125a68b3c10e2cbcfa8
jeyzth <jeyzth@gmail.com>
2016-03-03 23:41:16 +0200

    ticket:1 Done 5th item plan

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import sys
 from django.test import TestCase
 from django.test.client import Client
 
@@ -64,3 +65,83 @@ class AppicantTest(TestCase):
         self.assertIn(u"jeyzth@khavr.com", ucontent)
         self.assertNotIn(u"hodzha", ucontent)
         self.assertNotIn(u"nasredin", ucontent)
+
+    def test_page_ws_empty_table(self):
+        """ Check no model in the database
+        """
+        del_rec = Applicant.objects.all()
+        del_rec.delete()
+        c = Client()
+        response = c.get('http://localhost:8080')
+        ucontent = response.content.decode('utf8')
+        assert(ucontent.find(u"не знайдено жодного запису") > 0)
+
+    def test_correct_view_unicode(self):
+        """ This test check correct show unicode data
+        """
+        c = Client()
+        response = c.get('http://localhost:8080')
+        ucontent = response.content.decode('utf8')
+        self.assertIn(u"9 березня 1973 р.", ucontent)
+        self.assertIn(u"Євген", ucontent)
+        self.assertIn(u"Анонімов", ucontent)
+        self.assertIn(u"Працюю у сфері телекомунікацій", ucontent)
+        self.assertIn(u"бажано розробником на Python", ucontent)
+
+    def test_model_namesakes(self):
+        """ Check add few applicant with same Name and Surname
+        """
+        new_rec1 = Applicant(
+            name=u"Алдар",
+            surname=u"Косе",
+            dateofbird=u"1961-02-09",
+            bio="Хитрий і проворний",
+            email=u"aldar@satu.kz",
+            jabber=u"aldar@xmpp.kz",
+            skype=u"aldar.kose",
+            others="Хтозна"
+        )
+        new_rec2 = Applicant(
+            name=u"Алдар",
+            surname=u"Косе",
+            dateofbird=u"1968-02-09",
+            bio="Ще біль хитрий і проворний",
+            email=u"aldar2@satu.kz",
+            jabber=u"aldar2@xmpp.kz",
+            skype=u"aldar2.kose",
+            others="Хтозна"
+        )
+        new_rec1.save()
+        new_rec2.save()
+
+    def test_model_uniq_fields(self):
+        """ Check unique field of model
+        """
+        new_rec1 = Applicant(
+            name=u"Алдар",
+            surname=u"Косе",
+            dateofbird=u"1961-02-09",
+            bio="Хитрий і проворний",
+            email=u"aldar4@satu.kz",
+            jabber=u"aldar4@xmpp.kz",
+            skype=u"aldar4.kose",
+            others="Хтозна"
+        )
+        new_rec2 = Applicant(
+            name=u"Алдар",
+            surname=u"Косе",
+            dateofbird=u"1968-02-09",
+            bio="Ще біль хитрий і проворний",
+            email=u"aldar4@satu.kz",
+            jabber=u"aldar4@xmpp.kz",
+            skype=u"aldar4.kose",
+            others="Хтозна"
+        )
+        new_rec1.save()
+        try:
+            new_rec2.save()
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+        else:
+            print "test fail----------"
+            raise
