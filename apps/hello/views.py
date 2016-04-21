78b4035c775a8f04a5191a65f43a7429eb8704da
jeyzth <jeyzth@gmail.com>
2016-04-21 17:11:37 +0300

    ticket:1 Delete prit from tests.py Delete try in views.py as tell Reviwer

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -9,12 +9,8 @@ logger = logging.getLogger(__name__)
 
 
 def main_page(request):
-    try:
         applicant = Applicant.objects.first()
         logger.debug(applicant)
         context = {'applicant': applicant}
         logger.info('Get main_page')
         return render(request, 'hello/index.html', context)
-    except:
-        logger.info('Empty data in model')
-        return render(request, 'hello/emptytable.html', None)
