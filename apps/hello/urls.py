42a2ae671808a688508593e3575912bf382f3341
jeyzth <jeyzth@gmail.com>
2016-04-23 21:34:51 +0300

    ticket:3 Change url request10 to request .

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -5,6 +5,7 @@ import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.main_page, name='main_page'),
+    url(r'^requests$', views.requests10, name='requests10'),
     url(r'^requests10$', views.requests10, name='requests10'),
     url(r'^chknewreq$', views.chknewreq, name='chknewreq'),
 )
